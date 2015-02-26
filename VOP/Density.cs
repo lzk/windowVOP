ac3a79905b8b515bf9da300797175d3306d46d14
ShengFu <ShengFu.Li@liteon.com>
2015-02-26 16:25:19 +0800

    add Density Custom control.

---
+++ VOP/Density.cs
@@ -0,0 +1,104 @@
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Input; // for MouseButtonEventArgs
+using System.Windows.Media.Imaging; // for BitmapImage
+
+namespace VOP
+{
+
+    public partial class Density : UserControl
+    {
+        // Density value, from 1 to 6.
+        private int _density;
+        public int m_density
+        {
+            get
+            {
+                return _density;
+            }
+            set
+            {
+                if ( value >= 1 && value <= 6 )
+                {
+                    _density = value;
+
+                    SolidColorBrush br1 = new SolidColorBrush();
+                    SolidColorBrush br2 = new SolidColorBrush();
+                    br1.Color = Color.FromArgb(255, 0, 0, 0);
+                    br2.Color = Color.FromArgb(255, 128, 128, 128);
+
+                    rect1.Fill = br2;
+                    rect2.Fill = br2;
+                    rect3.Fill = br2;
+                    rect4.Fill = br2;
+                    rect5.Fill = br2;
+                    rect6.Fill = br2;
+                    
+                    switch ( _density )
+                    {
+                        case 1:
+                            rect1.Fill = br1;
+                            break;
+                        case 2:
+                            rect1.Fill = br1;
+                            rect2.Fill = br1;
+                            break;
+                        case 3:
+                            rect1.Fill = br1;
+                            rect2.Fill = br1;
+                            rect3.Fill = br1;
+                            break;
+                        case 4:
+                            rect1.Fill = br1;
+                            rect2.Fill = br1;
+                            rect3.Fill = br1;
+                            rect4.Fill = br1;
+                            break;
+                        case 5:
+                            rect1.Fill = br1;
+                            rect2.Fill = br1;
+                            rect3.Fill = br1;
+                            rect4.Fill = br1;
+                            rect5.Fill = br1;
+                            break;
+                        case 6:
+                            rect1.Fill = br1;
+                            rect2.Fill = br1;
+                            rect3.Fill = br1;
+                            rect4.Fill = br1;
+                            rect5.Fill = br1;
+                            rect6.Fill = br1;
+                            break;
+                    }
+                }
+            }
+        }
+
+        public Density()
+        {
+            InitializeComponent();
+
+            m_density = 3;
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+
+            Button srcButton = e.Source as Button;
+            if ( "btnIncrease" == srcButton.Name )
+            {
+                m_density++;
+            }
+            else if ( "btnDecrease" == srcButton.Name )
+            {
+                m_density--;
+            }
+
+        }
+
+    }
+}
