e53399c75c79752a70c61946774fbcd1f2131ed4
unknown <zhangqiangsunny@hotmail.com>
2015-03-02 16:25:17 +0800

    add groupbox control files

---
+++ VOP/Controls/GroupBoxDecorator/GroupBoxDecorator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+    public class GroupBoxDecorator : ContentControl
+    {
+
+        static GroupBoxDecorator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupBoxDecorator), new FrameworkPropertyMetadata(typeof(GroupBoxDecorator)));
+        }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+           DependencyProperty.Register("Header", typeof(string), typeof(GroupBoxDecorator));
+
+    
+    }
+}
---
+++ VOP/Controls/ImageButton/ImageButton.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImageButton.xaml
+    /// </summary>
+    public partial class ImageButton : UserControl
+    {
+        public ImageButton()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource ImagePath
+        {
+            get { return (ImageSource)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+
+        public string BottomText
+        {
+            get { return (string)GetValue(BottomTextProperty); }
+            set { SetValue(BottomTextProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register("ImagePath", typeof(ImageSource), typeof(ImageButton));
+
+        public static readonly DependencyProperty BottomTextProperty =
+            DependencyProperty.Register("BottomText", typeof(string), typeof(ImageButton));
+
+    }
+
+    public class EyeCandy
+    {
+
+        public static readonly DependencyProperty ImageProperty;
+
+
+        public static ImageSource GetImage(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(ImageProperty);
+        }
+
+
+        public static void SetImage(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(ImageProperty, value);
+        }
+
+
+        static EyeCandy()
+        {
+            //register attached dependency property
+            var metadata = new FrameworkPropertyMetadata((ImageSource)null);
+            ImageProperty = DependencyProperty.RegisterAttached("Image",
+                                                                typeof(ImageSource),
+                                                                typeof(EyeCandy), metadata);
+        }
+    }
+}
