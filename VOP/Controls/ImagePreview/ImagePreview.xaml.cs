ef883e89d1dda8d9e75bc48489e758570d987b32
unknown <zhangqiangsunny@hotmail.com>
2015-03-25 18:32:32 +0800

    Image preview TBC...

---
+++ VOP/Controls/ImagePreview/ImagePreview.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+    /// <summary>
+    /// Interaction logic for ImagePreview.xaml
+    /// </summary>
+    public partial class ImagePreview : UserControl
+    {
+        public ImagePreview()
+        {
+            InitializeComponent();
+        }
+
+        List<string> imagePaths = null;
+
+        public List<string> ImagePaths
+        {
+            set
+            {              
+                imagePaths = value;
+ 
+                if(imagePaths != null)
+                {
+
+                }
+            }
+            get
+            {
+                return imagePaths;
+            }
+        }
+    }
+}
---
+++ VOP/Controls/ImagePreview/ScrollableWidthConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+	public class ScrollableWidthConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return -(double)value;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+	}
+}
