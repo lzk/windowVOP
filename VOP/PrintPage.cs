d10f39fce9329ff977ca6b38b177c2ef62d69a4c
Ben <Ben.Weng@liteon.com>
2015-03-26 13:52:38 +0800

    add ValidateTip_TextBox

--- VOP/PrintPage.cs
+++ VOP/PrintPage.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Input; // for MouseButtonEventArgs
 using System.Windows.Media.Imaging; // for BitmapImage
+using System.Text.RegularExpressions;
 
 namespace VOP
 {
@@ -16,5 +17,28 @@ namespace VOP
             InitializeComponent();
         }
 
-     }
+        public bool ValidateText(string str)
+        {
+            if (0 != str.Length)
+            {
+                Regex rg = new Regex("^([0-9]{1,3})$");
+                if (rg.IsMatch(str))
+                {
+                    int value = int.Parse(str);
+
+                    if (!(0 <= value && value <= 100))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
 }
