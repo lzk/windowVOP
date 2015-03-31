ab2bc0b2610fcfd721dfb47f695d2ddd3973ae84
Kevin Yin <yinchen1105@126.com>
2015-03-31 08:34:50 +0800

    Modify wifi page layout.

--- VOP/WifiView.xaml.cs
+++ VOP/WifiView.xaml.cs
@@ -27,13 +27,35 @@ namespace VOP
 
         private void OnLoadWifiView(object sender, RoutedEventArgs e)
         {
+            wifilist.Children.Clear();
+
             VOP.Controls.WiFiItem wifiitem = new VOP.Controls.WiFiItem();
             wifiitem.SSIDText = "HJ-WLAN";
             wifiitem.EncryptionText = "通过WPA2进行保护";
             wifiitem.EncryptType = VOP.Controls.EnumEncryptType.MixedModePSK;
             wifiitem.WifiSignalLevel = VOP.Controls.EnumWifiSignalLevel.stronger;
 
-            wifiPanel.Children.Add(wifiitem);
+            wifilist.Children.Add(wifiitem);
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Name == "btnConnectOthAp")
+            {
+                btnConnectOthAp.Visibility = Visibility.Hidden;
+                manualConnect.Visibility = Visibility.Visible;
+                rowManual.Height = GridLength.Auto;
+                autoConnect.Visibility = Visibility.Hidden;
+            }
+            else if (btn.Name == "btnCancel")
+            {
+                btnConnectOthAp.Visibility = Visibility.Visible;
+                manualConnect.Visibility = Visibility.Hidden;
+                rowManual.Height = new GridLength(0); 
+                autoConnect.Visibility = Visibility.Visible;
+            }
 
         }
     }
