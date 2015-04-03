4f4a6383b20efdcc07a5356e7102626f7b79bfd2
Ben <Ben.Weng@liteon.com>
2015-04-03 16:55:19 +0800

    Add custom MessageBoxEx

---
+++ VOP/Controls/MessageBoxEx/MessageBoxEx_Simple.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxEx_Simple.xaml
+    /// </summary>
+    public partial class MessageBoxEx_Simple : Window
+    {
+        public MessageBoxEx_Simple(string messageBoxText = "", string caption = "")
+        {
+            InitializeComponent();
+
+            messageBoxTextBlock.Text = messageBoxText;
+            captionTextBlock.Text = caption;
+        }
+
+        public void Title_MouseButtonEventHandler(Object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ VOP/Controls/MessageBoxEx/MessageBoxEx_Video.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VOP.Controls
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxEx_1.xaml
+    /// </summary>
+    public partial class MessageBoxEx_Video : Window
+    {
+        public MessageBoxEx_Video(string messageBoxText ="", string caption="")
+        {
+            InitializeComponent();
+
+            messageBoxTextBlock.Text = messageBoxText;
+            captionTextBlock.Text = caption;
+            
+        }
+        public void Title_MouseButtonEventHandler(Object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
