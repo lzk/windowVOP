c64ffce4a51f267e1d83b4fa6ac65eb405254fd7
Kevin Yin <yinchen1105@126.com>
2015-04-29 13:48:27 +0800

    Fixed:#58964.

---
+++ VOP/Controls/PopupEx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace VOP.Controls
+{
+    public class PopupEx : Popup
+    {
+        public static DependencyObject GetPopupPlacementTarget(DependencyObject obj)
+        {
+            return (DependencyObject)obj.GetValue(PopupPlacementTargetProperty);
+        }
+  
+        public static void SetPopupPlacementTarget(DependencyObject obj, DependencyObject value)
+        {
+            obj.SetValue(PopupPlacementTargetProperty, value);
+        }
+ 
+        // Using a DependencyProperty as the backing store for PopupPlacementTarget.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PopupPlacementTargetProperty =
+            DependencyProperty.RegisterAttached("PopupPlacementTarget", typeof(DependencyObject), typeof(PopupEx), new PropertyMetadata(null, OnPopupPlacementTargetChanged));
+ 
+        private static void OnPopupPlacementTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                DependencyObject popupPopupPlacementTarget = e.NewValue as DependencyObject;
+                Popup pop = d as Popup;
+ 
+                Window w = Window.GetWindow(popupPopupPlacementTarget);
+                if (null != w)
+                {
+                    w.LocationChanged += delegate
+                    {
+                        var offset = pop.HorizontalOffset;
+                        pop.HorizontalOffset = offset + 1;
+                        pop.HorizontalOffset = offset;
+                    };
+                }
+            }
+        }
+
+        public static DependencyProperty TopmostProperty = Window.TopmostProperty.AddOwner(typeof(PopupEx), new FrameworkPropertyMetadata(false, OnTopmostChanged));
+        public bool Topmost
+        {
+            get { return (bool)GetValue(TopmostProperty); }
+            set { SetValue(TopmostProperty, value); }
+        }
+        private static void OnTopmostChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            (obj as PopupEx).UpdateWindow();
+        }
+        protected override void OnOpened(EventArgs e)
+        {
+            UpdateWindow();
+        }
+        private void UpdateWindow()
+        {
+            var hwnd = ((HwndSource)PresentationSource.FromVisual(this)).Handle;
+            RECT rect;
+            if (GetWindowRect(hwnd, out rect))
+            {
+                SetWindowPos(hwnd, Topmost ? -1 : -2, rect.Left, rect.Top, (int)(rect.Right - rect.Left), (int)(rect.Bottom - rect.Top), 0);
+            }
+        }
+        #region imports definitions
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32", EntryPoint = "SetWindowPos")]
+        private static extern int SetWindowPos(IntPtr hWnd, int hwndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+        #endregion
+    }
+}
