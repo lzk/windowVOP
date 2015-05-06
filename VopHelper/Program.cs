e7666d5af86c2544d2f0001a759e37934ac08430
ShengFu <shengfu.ccs.snnu@gmail.com>
2015-05-06 16:22:18 +0800

    Add VopHelper.

---
+++ VopHelper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VopHelper
+{
+    class Program
+    {
+        /// <summary>
+        /// Rotate bitmap file pathSrc and save to pathDst.
+        /// </summary>
+        private static void SaveRotatedImage( string pathSrc, string pathDst, string angle )
+        {
+            System.Drawing.Image i = System.Drawing.Image.FromFile( pathSrc );
+            i.RotateFlip( System.Drawing.RotateFlipType.Rotate90FlipNone );
+            i.Save( pathDst, System.Drawing.Imaging.ImageFormat.Bmp );
+        }
+        
+        static void Main(string[] args)
+        {
+            SaveRotatedImage( args[0], args[1], args[2] );
+        }
+    }
+}
