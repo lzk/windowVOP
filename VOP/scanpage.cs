37856684b2f66657aded157a112b63872a9a0332
ShengFu <shengfu.ccs.snnu@gmail.com>
2015-05-12 09:14:52 +0800

    Fixed #0059122.

--- VOP/scanpage.cs
+++ VOP/scanpage.cs
@@ -504,12 +504,11 @@ namespace VOP
                 
                 if ( false == DoseHasEnoughSpace(save.FileName) )
                 {
-                    // TODO: Update multi language. 
                     VOP.Controls.MessageBoxEx.Show(
                             VOP.Controls.MessageBoxExStyle.Simple,
                             m_MainWin,
                             (string)this.FindResource( "ResStr_Operation_cannot_be_carried_out_due_to_insufficient_memory_or_hard_disk_space_Please_try_again_after_freeing_memory_or_hard_disk_space_" ),
-                            (string)this.FindResource( "ResStr_Warning" )
+                            (string)this.FindResource( "ResStr_Error" )
                             );
 
                     return;
