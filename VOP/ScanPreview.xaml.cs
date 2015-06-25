541b056ac28787c68946383fedb4b6faf9fc5852
unknown <zhangqiangsunny@hotmail.com>
2015-06-25 11:47:53 +0800

    #59869, ScanPreview FitToWindow Scrollviewer change to ActualWidth

--- VOP/ScanPreview.xaml.cs
+++ VOP/ScanPreview.xaml.cs
@@ -298,8 +298,8 @@ namespace VOP
             double scaling2 = 0.0;
             double scaling0 = 0.0;
 
-            scaling1 = ( this.scrollPreview.ViewportWidth  -10 ) / previewImg.Source.Width;
-            scaling2 = ( this.scrollPreview.ViewportHeight -10 ) / previewImg.Source.Height;
+            scaling1 = ( this.scrollPreview.ActualWidth  -10 ) / previewImg.Source.Width;
+            scaling2 = ( this.scrollPreview.ActualHeight -10 ) / previewImg.Source.Height;
             scaling0 = (scaling1 < scaling2) ? scaling1 : scaling2;
 
             previewImg.Width  = previewImg.Source.Width * scaling0;
