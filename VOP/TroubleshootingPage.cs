e5dc31c707fa1d314fc9f25bb07fb6a58c213dc2
unknown <zhangqiangsunny@hotmail.com>
2015-08-14 16:25:01 +0800

    Add troubleshooting page uri files

--- VOP/TroubleshootingPage.cs
+++ VOP/TroubleshootingPage.cs
@@ -10,12 +10,19 @@ using VOP.Controls;
 using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Windows.Navigation;
+using System.Windows.Documents;
 
 namespace VOP
 {
     public partial class TroubleshootingPage : UserControl
     {
         public MainWindow m_MainWin { get; set; }
+
+        string help_file_usb = "USB.mht";
+        string help_file_wifi = "Wi-Fi.mht";
+
+        string help_path_En = @"Help\English\";
+        string help_path_CH = @"Help\SimplifiedChinese\";
 
         public TroubleshootingPage()
         {
@@ -25,9 +32,23 @@ namespace VOP
         private void OnClickImageButton(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             VOP.Controls.ImageButton btn = sender as VOP.Controls.ImageButton;
+
+            string url = "";
+
+            if (App.LangId == 1033)
+            {
+                url = btn.Name == "UsbLinkButton" ?
+                    help_path_En + help_file_usb : help_path_En + help_file_wifi;
+            }
+            else if (App.LangId == 2052) // zh-CN
+            {
+                url = btn.Name == "UsbLinkButton" ?
+                    help_path_CH + help_file_usb : help_path_CH + help_file_wifi;
+            }
+
             try
             {
-                System.Diagnostics.Process.Start(btn.Name == "UsbLinkButton" ? UsbLink.NavigateUri.ToString() : WiFilink.NavigateUri.ToString());
+                System.Diagnostics.Process.Start(url);
             }
             catch (Exception)
             {
@@ -37,14 +58,30 @@ namespace VOP
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
+            Hyperlink btn = sender as Hyperlink;
+
+            string url= "";
+
+            if (App.LangId == 1033)
+            {
+                url = btn.Name == "UsbLink" ? 
+                    help_path_En + help_file_usb : help_path_En + help_file_wifi;
+            }
+            else if (App.LangId == 2052) // zh-CN
+            {
+                url = btn.Name == "UsbLink" ?
+                    help_path_CH + help_file_usb : help_path_CH + help_file_wifi;
+            }
+
             try
             {
-                System.Diagnostics.Process.Start(e.Uri.ToString());
+                System.Diagnostics.Process.Start(url);
             }
             catch(Exception)
             {
 
             } 
         }
+
     }
 }
