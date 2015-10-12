86f3df08c248994c2bbf61823eb34f66d800563f
sonnyz <zhangqiangsunny@hotmail.com>
2015-10-12 15:26:22 +0800

    Change password window size

--- CRMUploader/Properties/AssemblyInfo.cs
+++ CRMUploader/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CRMUploader")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CRMUploader")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyProduct("Lenovo virtual panel")]
+[assembly: AssemblyDescription("0a35d5bff45d4e943234f8ef792c67958b61617c")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyCompany("Lenovo Co., Ltd.")]
+[assembly: AssemblyCopyright("(C) Lenovo Co., Ltd. (2015)")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e3cfdbb-1161-4abb-a16e-8cd8dffd4f88")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Lenovo virtual panel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.1")]
